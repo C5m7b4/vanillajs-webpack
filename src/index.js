84120fdab0acd8d1c4caadaa0fa7e785b3f4dad0
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-07 17:41:38 -0600

    introduce curry

--- src/index.js
+++ src/index.js
@@ -111,3 +111,18 @@ const deleteItem = (id) => {
     buildTable();
   }
 };
+
+// lets add curry to the mix
+const filterData = (property) => {
+  return function (value) {
+    return data.filter((i) => i[property] == value);
+  };
+};
+
+const curriedFilter = filterData('category');
+const fruits = curriedFilter('fruit');
+const bevs = curriedFilter('beverages');
+const candy = curriedFilter('candy');
+console.log('fruits', fruits);
+console.log('bevs', bevs);
+console.log('candy', candy);
