2b15093a93327bc1ab6d4a745f5013bbbc977ccf
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-07 16:16:39 -0600

    add state to the app:

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log('you are ready to start coding...');
 import { data } from './data';
+import { isValid } from './utils';
 import './styles.css';
 
 let filteredData = data;
@@ -13,3 +14,28 @@ const state = {
     category: '',
   },
 };
+
+const changeState = (identifier) => {
+  const { id, value } = identifier.target;
+  if (!isValid(value) || !isValid(id)) return;
+
+  setValue(id, value);
+
+  return {
+    ...state,
+    currentItem: {
+      ...(state.currentItem[id] = value),
+    },
+  };
+};
+
+const setValue = (identifier, value) => {
+  if (isValid(value)) {
+    document.getElementById(identifier).value = value;
+  }
+};
+
+const inputs = document.getElementsByTagName('input');
+for (let input of inputs) {
+  input.addEventListener('change', changeState);
+}
